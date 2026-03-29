4fc4c07ec428ee35e8aaca7075b68a2b9ff4778f
taudomite <taudomite@gmail.com>
2026-03-29 16:36:53 +0700

    update build

--- Dockerfile
+++ Dockerfile
@@ -3,8 +3,10 @@ WORKDIR /app
 
 RUN apk update
 RUN apk add --no-cache cmake build-base git && \
-    git clone https://github.com/10h30/lookbusy.git && \
+    git clone https://github.com/taudomite/lookbusy.git && \
     cd lookbusy && chmod +x ./configure && ./configure && make && \
     apk del build-base git cmake
+COPY start.sh /app/start.sh
+RUN chmod +x /app/start.sh
 
 CMD ["sh", "/app/start.sh"]
--- start.sh
+++ start.sh
@@ -1,2 +1,15 @@
-# /app/lookbusy/lookbusy -c 16-21 -r curve
-/app/lookbusy/lookbusy -c 0 -m 4000MB
+#!/bin/sh
+# Example: /app/lookbusy/lookbusy -c 16-21 -r curve
+# Usage: pass MB as first arg (e.g. "4000MB") or set LOOKBUSY_MB env var
+MB="${1:-${LOOKBUSY_MB:-4000MB}}"
+
+# If user passed only digits (e.g. "4000"), append the "MB" suffix.
+case "$MB" in
+	''|*[!0-9]*)
+		;;
+	*)
+		MB="${MB}MB"
+		;;
+esac
+
+/app/lookbusy/lookbusy -c 0 -m "$MB"
