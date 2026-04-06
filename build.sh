0356e75713b0a0e2b772fb8f215e0b342d92ed76
taudomite <taudomite@gmail.com>
2026-04-06 15:47:33 +0700

    update look

--- build.sh
+++ build.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
 set -e
 # build.sh - build the Docker image and run the container while passing
-# the MB value into /app/start.sh via the LOOKBUSY_MB environment variable.
+# the MB value into /app/start.sh via the LOOKBUS_MB environment variable.
 #
 # Usage:
 #   ./build.sh 4000MB         # pass MB as positional arg
-#   LOOKBUSY_MB=3000MB ./build.sh
+#   LOOKBUS_MB=3000MB ./build.sh
 #   ./build.sh               # defaults to 4000MB
 
-IMAGE_NAME="lookbusy-app"
-MB="${1:-${LOOKBUSY_MB:-4000MB}}"
+IMAGE_NAME="lookbus-app"
+MB="${1:-${LOOKBUS_MB:-4000MB}}"
 
 echo "Building Docker image: $IMAGE_NAME"
 docker build -t "$IMAGE_NAME" .
 
-echo "Running container (temporary) with LOOKBUSY_MB=$MB"
-docker run --rm -e LOOKBUSY_MB="$MB" "$IMAGE_NAME"
+echo "Running container (temporary) with LOOKBUS_MB=$MB"
+docker run -d --rm --name lookbus-cont -e LOOKBUS_MB="$MB" "$IMAGE_NAME"
 
-echo "Container exited (or was removed)."
+echo "Container is running in the background. It will exit automatically after 60 seconds."
--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # Example: /app/lookbusy/lookbusy -c 16-21 -r curve
-# Usage: pass MB as first arg (e.g. "4000MB") or set LOOKBUSY_MB env var
-MB="${1:-${LOOKBUSY_MB:-4000MB}}"
+# Usage: pass MB as first arg (e.g. "4000MB") or set LOOKBUS_MB env var
+MB="${1:-${LOOKBUS_MB:-4000MB}}"
 
 # If user passed only digits (e.g. "4000"), append the "MB" suffix.
 case "$MB" in
